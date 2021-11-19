c5145be6dccf1bad8ddaa96de61fca0640cff433
RajsJohal <rajsjohal97@hotmail.com>
2021-11-19 11:16:24 +0000

    Learned about Booleans and added collections file

---
+++ 01_Variables/Boolean_Intro.py
@@ -0,0 +1,27 @@
+# Boolean Data Type
+# a = True
+# b = False
+
+# print(a == b) # Should return False, Double = sign checks equivalence
+# print(a != b) # != Does not Equal to
+# print(5 < 6) # 5 is less than 6
+# print(5 > 6) # 5 is greater than 6
+# print(5 <= 6) # 5 is less than or equal to 6
+# print(5 >= 6) # 5 is greater than or equal to
+
+# print(a and b)
+# print(a or b)
+
+# hi = "Hello!"
+
+# print(hi.isalpha())
+# print(hi.upper().isupper())
+# print(hi.lower().islower())
+# print(hi.endswith("o"))
+# print(hi.startswith("H"))
+
+print(bool("2"))
+print(bool(""))
+
+n = None
+print(n is None)
