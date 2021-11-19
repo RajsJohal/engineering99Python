cb4a4fd3127d84070fc84e3bc55d99c298dd046f
RajsJohal <rajsjohal97@hotmail.com>
2021-11-19 12:18:29 +0000

    update on dictionary methods

--- 02_Collections/Dictionary_Intro.py
+++ 02_Collections/Dictionary_Intro.py
@@ -26,12 +26,27 @@ print(c)
 #Favourite Movie Dictionary
 
 fav_movie = {
+
     "Title": "Interstellar",
+    "Director": "Christopher Nolan",
     "Lead_Actor": "Matthew McConaughey",
     "Genre": "Sci-Fi",
     "Budget(USD)": 165000000,
     "Year of Release": 2014,
 }
+
+print(fav_movie, type(fav_movie))
+
+fav_movie["Supporting_Cast"] = ["Anne Hathaway", "Michael Cain", "Jessica Chastain"]
+print((fav_movie))
+
+print(fav_movie["Supporting_Cast"][-1]) #Access disctionary, access list within the dic and then print the last value within the list
+print(fav_movie.pop("Director"))
+print(fav_movie)
+
+print(fav_movie.keys())
+print(fav_movie.values())
+print(fav_movie.items()) # key value pair as a tuple
 
 
 
