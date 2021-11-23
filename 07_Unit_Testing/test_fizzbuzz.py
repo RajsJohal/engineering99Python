844bd846f7d6c32dcae37cf98b2b33520147e2a5
RajsJohal <rajsjohal97@hotmail.com>
2021-11-23 13:52:00 +0000

    Pytest with FizzBuzz

---
+++ 07_Unit_Testing/test_fizzbuzz.py
@@ -0,0 +1,19 @@
+from fizzbuzz import *
+
+#user input --> max number to hit
+#verify_digits
+
+
+# produces correct output
+
+def test_fizzbuzz_output_3_returns_fizz():
+    assert fizzbuzz_output(3) == "Fizz"
+
+def test_fizzbuzz_output_5_returns_buzz():
+    assert fizzbuzz_output(5) == "Buzz"
+
+def test_fizzbuzz_output_15_returns_buzz():
+    assert fizzbuzz_output(15) == "FizzBuzz"
+
+
+
